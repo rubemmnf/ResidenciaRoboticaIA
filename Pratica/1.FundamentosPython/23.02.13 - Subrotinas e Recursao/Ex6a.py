3d8a57dcedaf7af1262d288d9c29b9d5c8149d1b
Rubem Novellino <rmnf@cin.ufpe.br>
2023-02-14 16:48:11 -0300

    Update Ex6a.py

--- Pratica/1.FundamentosPython/23.02.13 - Subrotinas e Recursao/Ex6a.py
+++ Pratica/1.FundamentosPython/23.02.13 - Subrotinas e Recursao/Ex6a.py
@@ -1,6 +1,6 @@
-#Versão anterior
+# #Versão anterior
 
-#Variáveis
+# #Variáveis
 S = 0.0
 N = int(input("Insira N:"))
 i = 1
@@ -8,35 +8,52 @@ i = 1
 while N < 1 or N > 50:
     print("Inválido. Insira um número válido")
 
-#Solução com for
-#for i in range(1,N+1,1):
-#    if i % 2 == 1:
-#        aSomar = 2/(500 - (i-1)*10)
-#    else:
-#        aSomar = -5/(500 - (i-1)*10)
-#    S = S + aSomar
+# #Solução com for
+# #for i in range(1,N+1,1):
+# #    if i % 2 == 1:
+# #        aSomar = 2/(500 - (i-1)*10)
+# #    else:
+# #        aSomar = -5/(500 - (i-1)*10)
+# #    S = S + aSomar
 
 
-#print(fatorial)
+# #print(fatorial)
 
 #Solução com while
 while (i<=N):
     if i % 2 == 1:
         aSomar = 2/(500 - (i-1)*10)
     else:
-        aSomar = -5/(500 - (i-1)*10) 
+        aSomar = -5/(500 - (i-1)*10)
+    print(aSomar)
     S = S + aSomar
     i+=1
 
 print(S)
 
-#Como só alterna entre 2 e -5 poderia ser feito checando qual o valor atual
-# e se for um mudar para o outro da forma
+# #Como só alterna entre 2 e -5 poderia ser feito checando qual o valor atual
+# # e se for um mudar para o outro da forma
 
-nume = 2
-if nume == 2:
-    nume = -5
-else:
-    nume = 2
+# nume = 2
+# if nume == 2:
+#     nume = -5
+# else:
+#     nume = 2
 
-#Versão com recursividade
+# #Versão com recursividade
+
+def serie (n, nu = 2, de = 500):
+    res = nu/de
+    if n > 1:
+        if n % 2 == 0:
+            res += -5/2 * serie (n-1, nu, de - 10*n)
+        else:
+            res += serie (n-1, nu, de - 10*n)
+    print (res)
+    return res
+
+N = int(input("Insira N:"))
+while N < 1 or N > 50:
+    print("Inválido. Insira um número válido")
+
+print(serie(N))
